3d1bcde43e5a372d211d9363d0c4fba155d1541b
Kevin Krauss <earlkrauss@gmail.com>
2013-08-07 08:31:55 -0700

    trying to fix broken specs

--- spec/controllers/caught_exceptions_controller_spec.rb
+++ spec/controllers/caught_exceptions_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe CaughtExceptionsController do
 
   before(:each) do
+    @current_time = Time.now
     10.times do
       FactoryGirl.create(:caught_exception)
     end
@@ -28,23 +29,23 @@ describe CaughtExceptionsController do
     end
 
     it "should check a greater than match" do
-      get 'index', { :search_text => "created_at> #{Time.now - 1.hour}", :format => :json }
+      get 'index', { :search_text => "created_at> #{@current_time - 1.hour}", :format => :json }
       json_response = JSON.parse(response.body)
       json_response.size.should == 10
     end
 
     it "should check a less than match" do
-      get 'index', { :search_text => "created_at< #{Time.now}", :format => :json }
+      get 'index', { :search_text => "created_at< #{@current_time}", :format => :json }
       json_response = JSON.parse(response.body)
       json_response.size.should == 10
     end
 
     it "should check that many matchers work together" do
-      get 'index', { :search_text => "error_class= StandardError user_agent* Macintosh created_at> #{Time.now - 1.hour} created_at< #{Time.now}", :format => :json }
+      get 'index', { :search_text => "error_class= StandardError user_agent* Macintosh created_at> #{@current_time - 1.hour} created_at< #{@current_time}", :format => :json }
       json_response = JSON.parse(response.body)
       json_response.size.should == 10
 
-      get 'index', { :search_text => "error_class= StandardError error_message= StandardError: StandardError user_agent* Macintosh server_name* server created_at> #{Time.now - 1.hour} created_at< #{Time.now}", :format => :json }
+      get 'index', { :search_text => "error_class= StandardError error_message= StandardError: StandardError user_agent* Macintosh server_name* server created_at> #{@current_time - 1.hour} created_at< #{@current_time}", :format => :json }
       json_response = JSON.parse(response.body)
       json_response.size.should == 10
     end
