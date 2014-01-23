cd9a54eb268296fbb1c90a12f72af9e7c054b595
Kevin Krauss <earlkrauss@gmail.com>
2014-01-23 11:37:26 -0800

    getting close to having emailing with the configurations working

--- app/models/threshold.rb
+++ app/models/threshold.rb
@@ -1,8 +1,6 @@
 class Threshold
   include Mongoid::Document
   field :limit, type: Integer
-  field :error_class, type: String
-  field :controller, type: Hash
   field :raw_query, type: String
 
   embedded_in :project_configuration
