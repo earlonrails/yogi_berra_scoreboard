522870a5986082369caa0e67c51db35cad11230c
Kevin Krauss <earlkrauss@gmail.com>
2014-02-06 11:22:12 -0800

    removed the dsl type search for full text search branch

--- spec/controllers/caught_exceptions_controller_spec.rb
+++ spec/controllers/caught_exceptions_controller_spec.rb
@@ -14,40 +14,4 @@ describe CaughtExceptionsController do
     json_response = JSON.parse(response.body)
     json_response.size.should == 15
   end
-
-  context "match the search critera" do
-    it "should check an extact match" do
-      get 'index', { :search_text => "error_class= StandardError", :format => :json }
-      json_response = JSON.parse(response.body)
-      json_response.size.should == 15
-    end
-
-    it "should check a like match" do
-      get 'index', { :search_text => "user_agent* Macintosh", :format => :json }
-      json_response = JSON.parse(response.body)
-      json_response.size.should == 15
-    end
-
-    it "should check a greater than match" do
-      get 'index', { :search_text => "created_at> #{@current_time - 1.hour}", :format => :json }
-      json_response = JSON.parse(response.body)
-      json_response.size.should == 15
-    end
-
-    it "should check a less than match" do
-      get 'index', { :search_text => "created_at< #{@current_time}", :format => :json }
-      json_response = JSON.parse(response.body)
-      json_response.size.should == 15
-    end
-
-    it "should check that many matchers work together" do
-      get 'index', { :search_text => "error_class= StandardError user_agent* Macintosh created_at> #{@current_time - 1.hour} created_at< #{@current_time}", :format => :json }
-      json_response = JSON.parse(response.body)
-      json_response.size.should == 15
-
-      get 'index', { :search_text => "error_class= StandardError error_message= StandardError: StandardError user_agent* Macintosh server_name* server created_at> #{@current_time - 1.hour} created_at< #{@current_time}", :format => :json }
-      json_response = JSON.parse(response.body)
-      json_response.size.should == 15
-    end
-  end
 end
